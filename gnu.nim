6cdae6bf0b96fdab2428b646cd0f65a2c3d14c4a
qqtop <nomail@nomail.com>
2015-07-18 13:01:50 +0800

    ok

--- gnu.nim
+++ gnu.nim
@@ -1,17 +1,20 @@
-import os,terminal,private
+import os,terminal,private,strutils
 
 # The absolute must have gnu !
 
-proc gnuMe():string =
+
+proc gnuMe(j:int):string =
       eraseScreen()
       cursorUp(80)
-      rainbow("\nWWWWWW||WWWWWW\n W W W||W W W \n      || \n    ( OO )__________  \n     /  |          \\ \n    /o o| Niminator  \\ \n    \\___/||_||__||_||-* \n         || ||  || || \n        _||_|| _||_|| \n       (__|__|(__|__| ") 
+      rainbow("\n WWWWWW||WWWWWW\n  W W W||W W W \n       || \n     ( OO )__________  \n      /  |          \\ \n     /o o| Niminator  \\ \n     \\___/||_||__||_||-*  \n         || ||  || || \n         _||_|| _||_|| \n        (__|__|(__|__| ") 
       decho(2)
-      sleepy(1)
+      rainbow("   Gnu sightings :  ")
+      echo j
+      sleepy(0.15)
      
 
-for j in (0.. 10):
-    discard gnuMe()
-
+for j in 0.. 60:
+    discard gnuMe(j.int)
+    
 echo()
 
