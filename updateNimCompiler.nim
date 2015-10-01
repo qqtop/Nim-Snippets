928d5d17efa1ca846032c5f3b29a4a788f9040fd
qqtop <nomail@nomail.com>
2015-10-01 14:29:36 +0800

    ok

--- updateNimCompiler.nim
+++ updateNimCompiler.nim
@@ -1,32 +1,21 @@
-import os,strutils,parseopt2,terminal,times,strfmt,json,streams,httpclient
+import os,strutils,parseopt2,strfmt,json,httpclient,private
 
-# Reinstall Nim Development version only if build waterfall 
-# on nim-lang says "successfull"
-# 
-# suitable for linux only
-# 
-# based on linux install suggestion on
-# https://github.com/nim-lang/Nim
+# Reinstall Nim Development version  only if build waterfall says successfull
 
 ##############################################################
 # Change dirs as required
 
-var cp = "/data4/NimCompiler/"                   # where the compiler lives
+var cp = "/data4/NimCompiler/"   # where it the compiler lives
 var bsjson = "/data4/NimStuff/buildstatus.json"  # path for tmp file download
 
 ##############################################################
 
-let VERSION = "1.0"
-let start    = epochTime()
+let VERSION = "1.5"
 
-
-resetAttributes()
-eraseScreen()
-cursorUp(80) 
+clearup()
 
 const usageString = """Usage: updateNimCompiler [OPTIONS] /myinstall/path/
   
-
 Options:
                         Default Path /data4/NimCompiler/
                         Example : updateNimCompiler -i /myinstall/path/ 
@@ -35,35 +24,31 @@ Options:
     -h --help           print this help menu
     -i --install        install to path specified
 """  
- 
 
 var inCommands: seq[string] = @[]
-
-  
 inCommands.add(cp)  
+
 var iflag = false  
 
 var buildjson = "http://buildbot.nim-lang.org/json/builders/linux-x64-builder/builds?select=-1"
-
   
 proc checkBuildStatus(bss:string):bool =
-      var s : Stream
+      
       downloadFile(bss,bsjson)
       var jobj = parseFile(bsjson)
-      echo "Builder Name : " ,jobj["-1"]["builderName"].getstr
+      printLnBiCol("Builder Name : " & jobj["-1"]["builderName"].getstr,":")
       var cb : string = ""
       cb = $(jobj["-1"]["text"].getElems[1])
-      echo "Finished     : " , cb
       if cb == """"successful"""":
+         printLnBiCol("Finished     : " & cb,":")
          result = true
       else:
          result = false
-           
+          
   
 proc byebye() =
-    setForegroundColor(fgGreen) 
-    echo "\n",usageString
-    setForegroundColor(fgWhite)
+    echo() 
+    printG(usageString)
     quit(QuitFailure)
 
 
@@ -86,22 +71,19 @@ else:
 
 
 if not dirExists(cp):
-  echo  "Directory $1 must exist" % cp
+  printLn("Directory $1 must exist" % cp)
   byebye()
   
 else:
- echo "Installing Nim compiler to " & cp  
+ superHeader("Installing Nim compiler into :  " & cp) 
  echo ""
-
 
 setcurrentdir(cp)
 
 if checkBuildStatus(buildjson) == true :
-   
-    setForegroundColor(fgGreen)
-    echo "\n\n Ok we update"
-    setForegroundColor(fgWhite)
-
+  
+    decho(2)
+    printG("Ok we update")
     discard os.execShellCmd("rm -rf Nim") 
     discard os.execShellCmd("git clone git://github.com/Araq/Nim.git")
     setcurrentdir(cp&"Nim")
@@ -113,27 +95,13 @@ if checkBuildStatus(buildjson) == true :
     discard os.execShellCmd("./koch boot -d:release")
 
 else:
-  setForegroundColor(fgRed)
-  echo " \n\n BuildStatus = false . Try update later "
-  setForegroundColor(fgWhite)  
+  
+  decho(2)
+  printLnWonR(" Nim Waterfall BuildStatus : Failed .  Try update later ")
+  decho(2)
+    
 
 # clean up
 removeFile(bsjson)
-   
-when isMainModule:
-  # display some info, reset attribs and exit
-  
-  setForegroundColor(fgCyan,false)
-  let duration = epochTime() - start
-  echo "{}{}{}".fmt("\nRequest Duration              : ",duration.formatFloat(ffDecimal,5)," secs")  
-  echo "{}{:<11}{:>9}".fmt("Last module compilation on    : ",CompileDate ,CompileTime)  
-  setForegroundColor(fgGreen,false)   
-  echo "Programmed by                 : qqTop"
-  eraseLine()
-  setForegroundColor(fgGreen,false)
-  echo "{}{:<11}{:>9}".fmt("Current time                  : ",getDateStr(),getClockStr())
-  setForegroundColor(fgWhite,false)
-  echo "Finished "
-  
-  system.addQuitProc(resetAttributes)
-  quit 0
+ 
+doFinish()
