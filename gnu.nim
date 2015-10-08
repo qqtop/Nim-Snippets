b88390bc811ec0b4d746eadc42eb5f10e3d95e78
qqtop <nomail@nomail.com>
2015-10-08 19:51:18 +0800

    ok

--- gnu.nim
+++ gnu.nim
@@ -1,20 +1,18 @@
-import os,terminal,private,strutils
+import private,strutils
 
 # The absolute must have gnu !
 
 
 proc gnuMe(j:int):string =
-      eraseScreen()
-      cursorUp(80)
+      clearup()
       rainbow("\n WWWWWW||WWWWWW\n  W W W||W W W \n       || \n     ( OO )__________  \n      /  |          \\ \n     /o o| Niminator  \\ \n     \\___/||_||__||_||-*  \n         || ||  || || \n         _||_|| _||_|| \n        (__|__|(__|__| ") 
       decho(2)
-      rainbow("   Gnu sightings :  ")
-      echo j
+      printB(" Professional Gnu sightings :  ")
+      printLnCbonBlb($j)
       sleepy(0.15)
      
 
 for j in 0.. 60:
     discard gnuMe(j.int)
-    
-echo()
+doFinish()
 
