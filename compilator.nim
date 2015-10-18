71dfc942326dfa1c491a65abc1e3cfd81fd6c335
qqtop <nomail@nomail.com>
2015-10-18 12:31:06 +0800

    ok

--- compilator.nim
+++ compilator.nim
@@ -2,78 +2,53 @@
 # compilator.nim
 
 # compilator compiles all *.nim files in current dir
-# use after a compiler update to recompile all snippets
-# and test progs 
+# use after a compiler update to recompile all snippets and test progs 
 #
+# this proc can most likely not be stopped with ctrl-c  use ctrl-z instead
+# 
 # 
 
+import os,private,strutils,osproc,strfmt
 
-import os,strutils,osproc,terminal,times,strfmt
-
-
-
-proc handler*() {.noconv.} =
-    ## handler
-    ##
-    ## this runs if ctrl-c is pressed
-    ##
-    ## and provides some feedback upon exit
-    ##
-    ## just by using this library your project will have an automatic
-    ##
-    ## exit handler via ctrl-c
-    eraseScreen()
-    echo()
-    echo "Thank you for using     : ",getAppFilename()
-    echo "{}{:<11}{:>9}".fmt("Last compilation on     : ",CompileDate ,CompileTime)
-    echo "Nim Version             : ", NimVersion
-    echo()
-    system.addQuitProc(resetAttributes)
-    quit(0)
-
-
-setControlCHook(handler) 
-var start = epochTime() 
+# change below if there are different compiler settings required
+var cc = "nim -d:release --opt:speed --hints:off --verbosity:0 -w:off c " 
 var c = 0 
+var pc = 0 # file counter
 var failed = newSeq[string]()
 for f in walkfiles("*.nim"):
-  setForegroundColor(fgGreen)
-  echo " Working on : ",f
-  setforegroundcolor(fgWhite)
-  # change below if there are different compiler settings required
-  var exitCode = execCmd("nim -d:release --opt:speed --hints:off --verbosity:0 -w:off c " & f) 
+  inc pc
+  var compilecommand = cc & f
+  
+  printLnStyled(" Compiling file " & $pc & " : " & $f ,"",olivedrab,{stylereverse})
+  echo()
+  
+  var exitCode = execCmd(compilecommand) 
   
   if exitCode == 0 :
-      setforegroundcolor(fgWhite)
-      echo "exitCode : ",exitCode
-      setforegroundcolor(fgWhite)
+      cechoLn(yellowgreen,"exitCode : ",exitCode)
       inc c
   else:
-      setforegroundcolor(fgRed)
-      echo "exitCode : ",exitCode ,"  File : ",f,"  failed "
-      setforegroundcolor(fgWhite)    
+      cechoLn(red,"exitCode : ",exitCode ,"  File : ",f,"  failed ")
       
   if exitCode > 0:
       failed.add(f)
+  
+  hline()
+  decho(2)
+  
      
-     
-echo "\n\nCompilator finished in ",epochTime() - start," secs"
 echo()
-setforegroundcolor(fgYellow)
-echo "Failed compile attempts :"
+cechoLn(yellow,"Failed compile attempts :")
 echo ()
-setforegroundcolor(fgWhite)
 for x in failed:
   echo x
   
 echo()
-echo "Failed compilations : ",failed.len
-echo "Passed compilations : ",c
-echo ()
-echo "NimVersion : " ,NimVersion
-echo ()
+superheader("Total Compile attempts : " & $pc & "  Passed : " & $c  &  "  Failed : " & $failed.len)
+printLn("All files compiled with : ")
+printLn(cc,pastelyellow)
+
+doFinish() 
  
-system.addQuitProc(resetAttributes)
-quit 0  
 
    
