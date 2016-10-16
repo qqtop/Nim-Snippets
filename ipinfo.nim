38c25175f93afa1f4edf742a4afbddc301133e76
qqtop <nomail@nomail.com>
2016-10-16 17:31:01 +0800

    ok

--- ipinfo.nim
+++ ipinfo.nim
@@ -1,12 +1,9 @@
 import cx,osproc
 
-var z = tupletostr(execCmdEx("ip route | grep src"))
-var zz = z.split("src")
-var zzz = zz[1].split(", 0")  # keep space after comma  !!
 
 println("IP Info",salmon,styled={styleunderScore})
 echo()
-printlnBiCol("Current Local Ip:" & zzz[0])
+printlnBiCol("Current Local Ip:" & localIp())
 showWanIP()
 
 doFinish()
