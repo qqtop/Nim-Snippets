8d2e657bd063091a76db1d044008a9285ba3a5ff
qqtop <pict100@gmail.com>
2017-09-03 16:42:15 +0800

    Tiny improvements

---
+++ mecabT1.nim
@@ -0,0 +1,35 @@
+
+import pythonize
+
+# Example of running mecab via python with Nim  
+
+var nimkata = ""
+var mecab = ""
+echo()
+echo()
+var jtext = "打力全盛の高校野球、投手の逆襲に期待"
+
+echo "Mixed kanji , hiragana text " , " -->  ",jtext
+echo()
+
+proc doMecab(b:string):auto =
+        pythonEnvironment["text"] = b
+        execPython("mecab = MeCab.Tagger('-Oyomi')")
+        execPython("kata  = mecab.parse(text)")
+        nimkata = pythonEnvironment["kata"].depythonify(string)
+        execPython("hira = kata2hira(kata)")
+        execPython("mecab = MeCab.Tagger ()")
+        execPython("print mecab.parse(text)")
+             
+        result = pythonEnvironment["hira"].depythonify(string)
+        
+
+# set up for japanese
+execPython("import MeCab")
+execPython("from jcconv import kata2hira")
+
+echo "Hiragana : ", doMecab(jtext)
+echo "katakana : ", nimkata
+echo "Nim speaks japanese , with a little help from python"
+echo()
+
