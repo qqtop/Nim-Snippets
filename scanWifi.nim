7e058ed14d1a0f564dd6c3403cb567a3abf5af3f
qqtop <nomail@nomail.com>
2019-03-10 20:02:21 +0800

    tiny improvements

--- scanWifi.nim
+++ scanWifi.nim
@@ -1,51 +1,75 @@
-import nimcx,os,osproc
+import nimcx
 
-# wifi scanner  (requires root password)
+# wifi scanner  (may require root password)
+# for linux only
 # requires iwlist and iw installed
-# pass in your wifi interface to obtain info about other wifi routers in the vicinity
-
-# usage : scanWifi wlp6s0     (or any other wifi interface, wlp6s0 is used as default if no other interface given)
+# try's to automatically dedect you wifi interface wifi interface
+# to obtain info about other wifi routers in the vicinity
+# Usage   : scanWifi 
+# Last    : 2019-03-10
+# 
 
 proc scan(wifiinterface:string) = 
+  
    let (outp,errc) = execCmdEx("sudo iwlist $1 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID'" % wifiinterface)
    
    if errc == 0:
      for line in outp.splitlines():
         if line.contains("Cell "):
             echo()
-            printLnBiCol(line.strip(),lightseagreen,termwhite,":",60,false,{styleReverse})
+            printLnBiCol(line.strip(),colLeft=lightseagreen,xpos = 1,styled={styleReverse})
         elif line.contains("key:off"):
            printLn(line,red)
         elif line.contains("key:on")  :
            printLn(line,yellowgreen)
         elif line.contains("ESSID")  :
-           printLnBiCol(line,":",peru,termwhite,":",0,false,{})
+           printLnBiCol(line,colLeft=peru)
         else:
            printLn(line)
    else:
-        printLn("iwlist returned errorcode : " & $errc & " for interface : " & wifiinterface,red)
-      
+        echo()
+        printLnErrorMsg("iw returned errorcode : " & $errc & " for interface : " & wifiinterface,xpos = 1)
+        decho(2)
+   printLn("====   End of interface scan output for  $1   ====" % wifiinterface,xpos = 1)  
+   decho()
 
 proc signalStrength(wifiinterface:string) =
    # sorted list of signal strength of routers in vicinty
    # requires iw
-   printLn("\n\nVicinity routers sorted by strength , strongest on top",yellowgreen)
+   decho(2)
+   printLn("Routers & Access Points strongest signal on top     ",yellowgreen,xpos = 1,styled={styleUnderscore})
    echo()
-   let (outp,errc) = execCmdEx("""sudo iw dev $1 scan | egrep "signal|SSID" | sed -e "s/\tsignal: //" -e "s/\tSSID: //" | awk '{ORS = (NR % 2 == 0)? "\n" : " "; print}' | sort"""  % wifiinterface)
+   let (outp,errc) = execCmdEx("""sudo iw dev $1 scan | egrep "signal|SSID" | sed -e "s/\tsignal: //" -e "s/\tSSID: //" | awk '{ORS = (NR % 2 == 0)? "\n" : " "; print}' | sort""" % wifiinterface)
    if errc == 0:
-      echo outp
+      var op = outp.splitLines()
+      for aline in op:
+          printLn(aline,pastelpink,xpos = 1)
    else:
-        printLn("iw returned errorcode : " & $errc & " for interface : " & wifiinterface,red)
-
+         decho()
+         printLnErrorMsg("iw returned errorcode : " & $errc & " for interface : " & wifiinterface,xpos = 1)
+   printLn("====   End of signal strength output ====",xpos = 1)
+   decho(2)
 
 # run the scans
 
-var z = ""      
-try:
-  z = paramStr(1)
-except IndexError:
-  z = "wlp6s0"
-scan(z)
-signalStrength(z)
-
-doFinish()
+proc main() =
+    printLn("scanWifi    -     Wifi Interface Scan  " & spaces(40),yellowgreen,xpos = 1,styled={styleUnderscore}) 
+    decho(2)
+    let (di,de) = execCmdEx("sudo ip route | grep default")
+    if de == 0:
+         let zz = di.splitLines()
+         for zzz in 0 ..< zz.len - 1:
+             printLnBiCol("Testing Interface : " & zz[zzz],colLeft=yellowgreen,xpos = 1)
+             try:
+                 let zzzzz = zz[zzz].split("dev ")[1].split(" proto")[0].strip()
+                 # now check if this is a wifi interface  
+                 scan(zzzzz)
+                 signalStrength(zzzzz)
+             except:
+                  discard
+    else:
+          printLnErrorMsg("Are ip , iw ,iwlist installed on your linux system ?")
+          
+when isMainModule: 
+    main()
+    doFinish()
